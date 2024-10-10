d09c0b9d28be4011688598fb2143d3d22c35e962
ganciaux <ghislain.anciaux@live.fr>
2024-10-10 16:59:37 +0200

    Start get form list from service

--- src/app/core/features/form/components/form-list/form-list.component.ts
+++ src/app/core/features/form/components/form-list/form-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Form } from '@angular/forms';
+import {FormService} from "../../services/form.service";
 
 @Component({
   selector: 'app-form-list',
@@ -8,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './form-list.component.css'
 })
 export class FormListComponent {
+  forms = signal<Form[]>([]);
+  formService = inject(FormService);
 
+  constructor(){
+    this.loadForms()
+      .then(() => console.log(`All courses loaded:`, this.forms()));
+  }
+
+  async loadForms() {
+    try {
+      const forms = await this.formService.loadForm({});
+      this.forms.set(forms);
+    }
+    catch(err) {
+      console.error(err);
+    }
+  }
 }
--- src/app/core/features/form/services/form.service.ts
+++ src/app/core/features/form/services/form.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Form } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
+  env = environment;
+
+  http = inject(HttpClient);
+
   constructor() { }
+
+  async loadForm(config: {
+    formId?:string,
+    query?:string;
+  }): Promise<Form[]> {
+    const {formId, query} = config;
+    let params = new HttpParams();
+    if (formId) {
+      params = params.set("formId", formId);
+    }
+    if (query) {
+      params = params.set("query", query);
+    }
+    const forms$ = this.http.get<Form[]>(
+      `${this.env.apiRoot}/search-lessons`,
+      {
+        params
+      }
+    )
+    const response = await firstValueFrom(forms$);
+    return response;
+  }
+
 }
