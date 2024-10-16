0d8cdffa5bd54f1a3dd35b2a97e7306dda383088
ganciaux <ghislain.anciaux@live.fr>
2024-10-16 10:43:12 +0200

    Add new forms, load form from id

--- src/app/core/features/form/components/form-edit/form-edit.component.ts
+++ src/app/core/features/form/components/form-edit/form-edit.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core'
+import { Form } from '../../models/form.model'
+import { HttpClient } from '@angular/common/http'
+import { ActivatedRoute } from '@angular/router'
+import { FormService } from '../../services/form.service'
+import { JsonPipe } from '@angular/common'
 
 @Component({
   selector: 'app-form-edit',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './form-edit.component.html',
-  styleUrl: './form-edit.component.css'
+  styleUrl: './form-edit.component.css',
 })
 export class FormEditComponent {
+  form = signal<Form | null>(null)
+  httpClient = inject(HttpClient)
+  route = inject(ActivatedRoute)
+  formService = inject(FormService)
 
+  constructor() {
+    this.loadForm().then(() => console.log('loading form...'))
+  }
+
+  async loadForm() {
+    try {
+      const formId = this.route.snapshot.params['id']
+      if (formId) {
+        console.log("formId", formId)
+        const form = await this.formService.loadForm({ formId })
+        console.log(form);
+        this.form.set(form[0])
+      } else {
+        console.log("formId non trouvé dans l'URL")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
--- src/app/core/features/form/services/form.service.ts
+++ src/app/core/features/form/services/form.service.ts
@@ -1,41 +1,33 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Form } from "../models/form.model";
-import { firstValueFrom } from 'rxjs';
-import { environment } from '../../../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { inject, Injectable } from '@angular/core'
+import { Form } from '../models/form.model'
+import { firstValueFrom } from 'rxjs'
+import { environment } from '../../../../../environments/environment'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormService {
+  env = environment
 
-  env = environment;
+  http = inject(HttpClient)
 
-  http = inject(HttpClient);
+  constructor() {}
 
-  constructor() { }
-
-  async loadForm(config: {
-    formId?:string,
-    query?:string;
-  }): Promise<Form[]> {
-    const {formId, query} = config;
-    let params = new HttpParams();
+  async loadForm(config: { formId?: string; query?: string }): Promise<Form[]> {
+    const { formId, query } = config
+    let params = new HttpParams()
     if (formId) {
-      params = params.set("formId", formId);
+      params = params.set('id', formId)
     }
     if (query) {
-      params = params.set("query", query);
+      params = params.set('query', query)
     }
     console.log(this.env.apiRoot)
-    const forms$ = this.http.get<Form[]>(
-      `${this.env.apiRoot}`,
-      {
-        params
-      }
-    )
-    const response = await firstValueFrom(forms$);
-    return response;
+    const forms$ = this.http.get<Form[]>(`${this.env.apiRoot}`, {
+      params,
+    })
+    const response = await firstValueFrom(forms$)
+    return response
   }
-
 }
