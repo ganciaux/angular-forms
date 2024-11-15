6fc5999c33a5b6b0b1b3cd6b8f33a80f7a0964ef
ganciaux <ghislain.anciaux@live.fr>
2024-11-15 13:42:51 +0100

    Add error handler

--- backend/src/main/java/com/ganciaux/forms/rest/StudentRestController.java
+++ backend/src/main/java/com/ganciaux/forms/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.ganciaux.forms.rest;
 
 import com.ganciaux.forms.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +27,9 @@ public class StudentRestController {
 
     @GetMapping("/students/{id}")
     public Student getStudent(@PathVariable int id){
+        if (id>=theStudents.size() || id<=0){
+            throw new StudentNotFoundException("Student id is not found");
+        }
         return theStudents.get(id);
     }
 }
